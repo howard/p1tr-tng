6f77c110ef69ba24e21397a447170bda385970f5
Christian Ortner <chris.ortner@gmail.com>
2012-07-17 20:30:31 +0200

    Added uptime and uname to the unix plugin.

--- p1tr/plugins/unix/unix.py
+++ p1tr/plugins/unix/unix.py
@@ -1,12 +1,30 @@
 from plugin import *
 import subprocess
 
-class Unix(Plugin):
+def get_command_output(command):
+        """Returns the output of a console command. Discards STDERR."""
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        stdout_value, stderr_value = process.communicate()
+        return ' '.join(stdout_value.decode('utf-8').split())
 
+class Unix(Plugin):
+    """Provides access to some classic unix commands."""
+    
     @command
     def fortune(self, server, channel, nick, params):
         """Prints a random fortune cookie."""
-        process = subprocess.Popen('fortune', shell=True, stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
-        stdout_value, stderr_value = process.communicate()
-        return ' '.join(stdout_value.decode('utf-8').split())
+        return get_command_output('fortune')
+
+    @command
+    def uname(self, server, channel, nick, params):
+        """
+        Displays information about the operating system, as provided by the
+        uname -a command.
+        """
+        return get_command_output('uname -a')
+
+    @command
+    def uptime(self, server, channel, nick, params):
+        """Prints operating system uptime."""
+        return get_command_output('uptime')
+
