72ca607b18c997db14245a4fbb0e1e4da22ef990
Chris Ortner <chris@codexfons.com>
2012-08-05 12:47:21 +0200

    Adapt plugin scaffold creation script to generate a dummy test case

--- scripts/mkplugin.sh
+++ scripts/mkplugin.sh
@@ -6,7 +6,8 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-# Since the bot home is optional, check if the parameter is set, and use the default if not.
+# Since the bot home is optional, check if the parameter is set, and use the
+# default if not.
 if [ $# -lt 2 ]; then
     BOT_HOME=.
 else
@@ -26,9 +27,8 @@ mkdir $PLUGIN_DIR
 touch $PLUGIN_DIR/__init__.py
 
 # Create dummy implementation
-PLUGIN_FILE=$PLUGIN_DIR/$PLUGIN_NAME.py
 CLASS_NAME="${PLUGIN_NAME[@]^}"
-cat << EOF | cat > $PLUGIN_FILE
+cat << EOF | cat > $PLUGIN_DIR/$PLUGIN_NAME.py
 from p1tr.plugin import *
 from p1tr.helpers import *
 
@@ -38,11 +38,43 @@ class $CLASS_NAME(Plugin):
     @command
     def myCommand(self, server, channel, nick, params):
         """
-        Command can be called via !myCommand, if ! is the signal character. This text
-        will be displayed when looking for help on this command.
+        Command can be called via !myCommand, if ! is the signal character. This
+        text will be displayed when looking for help on this command.
         """
-        return "This text is sent as a response to the command."
+        return 'This text is sent as a response to the command.'
 
+EOF
+
+# Create dummy test case
+cat << EOF | cat > $PLUGIN_DIR/$PLUGIN_NAME_test.py
+from p1tr.plugin import *
+from p1tr.test import *
+
+class $CLASS_NAMETest(PluginTestCase):
+    """This is a dummy test case for the generated plugin scaffold."""
+
+    def setUp(self):
+        super(PluginTestCase, self)
+        # Perform any setup task, but call the parent method first.
+
+    def tearDown(self):
+        super(PluginTestCase, self)
+        # Same goes for tearDown. If you don't need setUp or tearDown, you can
+        # just omit them, though.
+    
+    def myCommand_test(self):
+        """
+        This test runs the command with various parameters, checking if it is
+        fit for extreme cases.
+        """
+        expected = 'This text is sent as a response to the command.'
+        # Dummy values for common parameters are included in this class. There
+        # are several for each. You can check all of them using a loop.
+        # Also, you already have an instance of the plugin available:
+        for data in self.dummy_data:
+            ret_val = self.plugin.myCommand(data.server, data.channel,
+                    data.nick, data.params)
+            self.assertEqual(expected, ret_val)
 EOF
 
 echo "Develop your plugin by editing the file $PLUGIN_FILE."
