864a6285d0004943187a36b364b16efdd925d5e6
Christian Ortner <chris@codexfons.com>
2012-08-09 19:54:54 +0200

    Fix intentionally broken test case in seen plugin tests and add remaining ones

--- plugins/seen/seen_test.py
+++ plugins/seen/seen_test.py
@@ -54,4 +54,52 @@ class SeenTest(PluginTestCase):
     @test
     def seen_part_test(self):
         """Test for a target that has been seen on part."""
-        self.assertTrue(False)
+        data = self.dummy_data[0]
+        self.plugin.on_userpart(data.server, data.channel, data.nick,
+                'so long, and thanks for all the fish!')
+        self.assertEqual(self.plugin.seen(data.server, data.channel,
+                    self.dummy_data[1].nick, data.nick.split('!')[0]),
+                '%s was last seen 0 seconds ago in %s, leaving the channel, \
+saying "so long, and thanks for all the fish!".' % (
+                    data.nick.split('!')[0], data.channel))
+    @test
+    def seen_part_test2(self):
+        """Test for a target that has been seen on part without part message."""
+        data = self.dummy_data[0]
+        self.plugin.on_userpart(data.server, data.channel, data.nick,
+                '')
+        self.assertEqual(self.plugin.seen(data.server, data.channel,
+                    self.dummy_data[1].nick, data.nick.split('!')[0]),
+                '%s was last seen 0 seconds ago in %s, leaving the channel.' % (
+                    data.nick.split('!')[0], data.channel))
+
+    @test
+    def seen_kick_test(self):
+        """Test for a target that has been seen on kick."""
+        data = self.dummy_data[0]
+        self.plugin.on_userkicked(data.server, data.channel, data.nick,
+                'he acted silly')
+        self.assertEqual(self.plugin.seen(data.server, data.channel,
+                    self.dummy_data[1].nick, data.nick.split('!')[0]),
+                '%s was last seen 0 seconds ago in %s, getting kicked because: \
+he acted silly.' % (data.nick.split('!')[0], data.channel))
+
+    @test
+    def seen_kick_test2(self):
+        """Test for a target that has been seen on kick without a reason"""
+        data = self.dummy_data[0]
+        self.plugin.on_userkicked(data.server, data.channel, data.nick, '')
+        self.assertEqual(self.plugin.seen(data.server, data.channel,
+                    self.dummy_data[1].nick, data.nick.split('!')[0]),
+                '%s was last seen 0 seconds ago in %s, getting kicked.' % (
+                    data.nick.split('!')[0], data.channel))
+
+    @test
+    def seen_rename_test(self):
+        """Test for a target that has been seen renaming."""
+        data = self.dummy_data[0]
+        self.plugin.on_userrenamed(data.server, data.nick, 'Arthur')
+        self.assertEqual(self.plugin.seen(data.server, data.channel,
+                    self.dummy_data[1].nick, data.nick.split('!')[0]),
+                '%s was last seen 0 seconds ago in some channel, changing his \
+nick to Arthur.' % (data.nick.split('!')[0]))
